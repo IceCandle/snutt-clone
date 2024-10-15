fda93425d4ea6a04c05fd8efb805de7f929a7cba
IceCandle <icecandle2413@gmail.com>
2024-10-15 20:41:48 +0900

    line extending to widescreen fix

--- src/components/SocialLoginButtons.tsx
+++ src/components/SocialLoginButtons.tsx
@@ -9,13 +9,13 @@ const SocialLoginButtons = () => {
   };
 
   return (
-    <div className="w-full px-3 mt-10">
-      <div className="w-full max-w-sm mt-8 flex items-center justify-between px-4">
-        <div className="flex-grow h-px bg-[#c4c4c4]"></div>
-        <span className="px-2 text-sm text-[#c4c4c4] font-medium">
+    <div className="w-full max-w-sm mx-auto px-3 mt-10 flex-col justify-start items-start gap-2.5">
+      <div className="h-[17px] flex justify-center items-center gap-2.5">
+        <div className="grow shrink basis-0 h-[0px] bg-[#c4c4c4] border border-[#c4c4c4]"></div>
+        <div className="text-[#c4c4c4] text-sm font-medium font-['Pretendard']">
           SNS 계정으로 계속하기
-        </span>
-        <div className="flex-grow h-px bg-[#c4c4c4]"></div>
+        </div>
+        <div className="grow shrink basis-0 h-[0px] bg-[#c4c4c4] border border-[#c4c4c4]"></div>
       </div>
       <div className="mt-4 flex justify-center items-center gap-3">
         <img
